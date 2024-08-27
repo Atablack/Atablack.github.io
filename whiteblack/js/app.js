d41d6b8eed307c5012bfac9b3a37d267dddc966d
Atablack <79922783+Atablack@users.noreply.github.com>
2024-08-27 11:19:23 +0200

    Add files via upload

---
+++ whiteblack/js/app.js
@@ -0,0 +1,12 @@
+alert('luv u')
+let bt = document.querySelector('.cl')
+let hide = document.querySelector('.button')
+let swiperShow = document.querySelector('.ko')
+
+
+let show = () => {
+    swiperShow.style.display = 'block'
+    hide.style.display = 'none'
+}
+
+bt.addEventListener('click', show)  
